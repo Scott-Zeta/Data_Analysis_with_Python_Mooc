b3d1ba84a8f81f43bedd3cc3171b1b1793ec57b6
Scott-Zeta <wc070211@gmail.com>
2022-11-19 16:46:36 +1030

    config environment

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e01_hello_world/src/hello_world.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e01_hello_world/src/hello_world.py
@@ -11,7 +11,7 @@
 
 def main():
     # Enter your code here, this is the correct indentation
-
+    print("Hello, world!")
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
 if __name__ == "__main__": 
