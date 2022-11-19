da364af13e8ef75c5096b1330c64782d4535e48e
Scott-Zeta <wc070211@gmail.com>
2022-11-19 18:40:37 +1030

    end of day1

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e03_multiplication/src/multiplication.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e03_multiplication/src/multiplication.py
@@ -2,6 +2,7 @@
 
 def main():
     # Enter your solution here
-
+    for i in range(11):
+        print(4,"multiplied by", i, "is", 4*i)
 if __name__ == "__main__":
     main()
--- tmcdata/mooc-data-analysis-with-python-2022/part01-e04_multiplication_table/src/multiplication_table.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,10 @@
 
 
 def main():
-    pass
+    for i in range(1,11):
+        for j in range(10):
+            print(i+j*i, end=" ")
+        print("")
 
 if __name__ == "__main__":
     main()
