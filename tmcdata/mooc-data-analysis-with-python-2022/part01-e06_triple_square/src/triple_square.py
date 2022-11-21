49afa4002051165ac6a3233b8492c3397cdff0de
Scott-Zeta <wc070211@gmail.com>
2022-11-21 17:38:01 +1030

    ex6

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e06_triple_square/src/triple_square.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+def triple(num):
+    return num*3
+
+def square(num):
+    return num ** 2
 
 def main():
-    pass
+    for i in range(1,11):
+        s = square(i)
+        t = triple(i)
+        if(t >= s):
+            print(f"triple({i})=={t} square({i})=={s}")
+        else:
+            break
 
 if __name__ == "__main__":
     main()
