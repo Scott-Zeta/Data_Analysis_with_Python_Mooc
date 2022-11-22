254532f37bffcf45153407b2dbd7a9425ff162eb
Scott-Zeta <wc070211@gmail.com>
2022-11-22 19:30:17 +1030

    ex9

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e09_merge/src/merge.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e09_merge/src/merge.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    newlist = []
+    p1 = 0
+    p2 = 0
+    while(p1 < len(L1) and p2 < len(L2)):
+        if(L1[p1] < L2[p2]):
+            newlist.append(L1[p1])
+            p1+=1
+        else:
+            newlist.append(L2[p2])
+            p2+=1
+    if(p1<len(L1)):
+            newlist.extend(L1[p1:])
+    if(p2<len(L2)):
+            newlist.extend(L2[p2:])
+    return newlist
 
 def main():
-    pass
+    list1 = [1,7,9,10]
+    list2 = [0,2,4,6,15,20]
+    print(merge(list1,list2))
 
 if __name__ == "__main__":
     main()
