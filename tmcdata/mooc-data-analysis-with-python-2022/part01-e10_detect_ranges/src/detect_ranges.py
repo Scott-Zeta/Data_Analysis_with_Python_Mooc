6c6a4857f26021457e4fc1b66e1568e1a5ff4e2b
Scott-Zeta <wc070211@gmail.com>
2022-11-23 10:48:13 +1030

    ex10

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e10_detect_ranges/src/detect_ranges.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    rangeList = sorted(L)
-    parser = []
+    sortedList = sorted(L)
+    rangeList = []
+    p1 = 0
+    p2 = 0
+    while(p1 < len(sortedList) and p2 < len(sortedList)):
+        if(p2 < len(sortedList) -1  and sortedList[p2] + 1 == sortedList[p2 + 1]):
+            p2+=1
+        else:
+            if(p1 == p2):
+                rangeList.append(sortedList[p1])
+            else:
+                rangeList.append((sortedList[p1],sortedList[p2] + 1))
+            p2 += 1
+            p1 = p2
+        print(rangeList)
     return rangeList
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
+    print(sorted(L))
     result = detect_ranges(L)
     print(L)
     print(result)
