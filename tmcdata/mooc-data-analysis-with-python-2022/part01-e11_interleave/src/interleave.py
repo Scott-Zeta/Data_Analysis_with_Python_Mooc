e275218fba9646dd7293ffff25cfd4b49f9d1e56
Scott-Zeta <wc070211@gmail.com>
2022-11-23 12:27:57 +1030

    ex10 honest I don' know why should i use zip

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e11_interleave/src/interleave.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e11_interleave/src/interleave.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    x1= []
+    x2= []
+    zip(x1,x2)
+    result = []
+    for j in range(len(lists[0])):
+        for i in range(len(lists)):
+            result.append(lists[i][j])
+    return result
 
 def main():
-    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+    print(list(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c'])))
 
 if __name__ == "__main__":
     main()
