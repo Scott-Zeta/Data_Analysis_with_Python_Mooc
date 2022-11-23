81474c71bfe4c4379c424d2b31c9c4ac5f2f3d1d
Scott-Zeta <wc070211@gmail.com>
2022-11-23 16:05:17 +1030

    ex13

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    rDict = {}
+    for i in d:
+        # print(i)
+        for j in d[i]:
+            # print(j)
+            if j in rDict:
+                rDict[j].append(i)
+            else:
+                rDict.update({j:[i]})
+
+    return rDict
 
 def main():
-    pass
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
 
 if __name__ == "__main__":
     main()
