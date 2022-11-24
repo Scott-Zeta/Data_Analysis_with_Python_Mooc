cd026f96d05d803a2242ea25db00e7fd560cc434
Scott Zeta <wc070211@gmail.com>
2022-11-24 15:07:35 +1030

    ex15

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    result = [(i,j) for i in range(1,7)
+                    for j in range(1,7)
+                    if i+j ==5]
+    for p in result:
+        print(p)
 
 if __name__ == "__main__":
     main()
