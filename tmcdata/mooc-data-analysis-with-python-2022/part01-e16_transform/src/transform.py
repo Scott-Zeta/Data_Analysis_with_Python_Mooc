2aa6b73f3586f594fad1f165178b04d00d5052d8
Scott-Zeta <wc070211@gmail.com>
2022-11-25 18:35:14 +1030

    ex16

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e16_transform/src/transform.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e16_transform/src/transform.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+
 def transform(s1, s2):
-    return []
+    newlist = zip([int(i) for i in s1.split()],[int(i) for i in s2.split()])
+    # split default seperator is any white space " ", "\t" etc.
+    # print(list(map(lambda x: x[0]*x[1], newlist)))
+    return list(map(lambda x: x[0]*x[1], newlist))
 
 def main():
-    pass
+    result = transform("1 5 3", "2 6 -1")
+    print(result)
 
 if __name__ == "__main__":
     main()
