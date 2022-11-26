d9ab1dbeec10fa4a8943837e2894066b6790d458
Scott-Zeta <wc070211@gmail.com>
2022-11-26 19:19:02 +1030

    ex19

--- tmcdata/mooc-data-analysis-with-python-2022/part01-e19_sum_equation/src/sum_equation.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+from functools import reduce
 
 def sum_equation(L):
-    return ""
+    if(L == []):
+        return "0 = 0"
+    sum = reduce(lambda x,y:x+y, L, 0)
+    #print(sum)
+    stringL = [str(x) for x in L]
+    stringL = " + ".join(stringL)
+    return f"{stringL} = {sum}"
 
 def main():
-    pass
+    print(sum_equation([1,4,6,8,10]))
 
 if __name__ == "__main__":
     main()
