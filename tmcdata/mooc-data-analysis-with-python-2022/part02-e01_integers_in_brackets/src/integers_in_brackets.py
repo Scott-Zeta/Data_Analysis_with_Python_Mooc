f1ac7a11dfab16021de94ba316e7c64121df842c
Scott-Zeta <wc070211@gmail.com>
2022-11-30 17:10:06 +1030

    correct version, no need deal with multiple sign

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -2,25 +2,14 @@
 import re
 
 def integers_in_brackets(s):
-    matches = re.findall(r'\[[\s]*([-+]*)(\d+)[\s]*\]', s)
+    matches = re.findall(r'\[[\s]*([-+]{0,1}\d+)[\s]*\]', s)
     print(matches)
-    newTupleList = []
-    for elements in matches:
-        count = 0
-        for i in range(len(elements[0])):
-            if(elements[0][i] == "-"):
-                count += 1
-        if(count%2 == 0):
-            newTupleList.append((1,elements[1]))
-        else:
-            newTupleList.append((-1,elements[1]))
-    print(newTupleList)
-    result = [i[0] * int(i[1]) for i in newTupleList]
-    #print(result)
+    result = [int(i) for i in matches]
+    print(result)
     return result
 
 def main():
-    print(integers_in_brackets("afd [128+] [47 ] [a34]  [ +-43 ]tt [+12]xxx!"))
+    print(integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx [+-43]"))
 
 if __name__ == "__main__":
     main()
