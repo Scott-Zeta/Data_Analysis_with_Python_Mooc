0c5c17b3a5b9d59c617a06706b4fb32cdd057b36
Scott-Zeta <wc070211@gmail.com>
2022-11-30 17:13:09 +1030

    use ? instead of {0,1} to make it neat

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -2,7 +2,7 @@
 import re
 
 def integers_in_brackets(s):
-    matches = re.findall(r'\[[\s]*([-+]{0,1}\d+)[\s]*\]', s)
+    matches = re.findall(r'\[[\s]*([-+]?\d+)[\s]*\]', s)
     print(matches)
     result = [int(i) for i in matches]
     print(result)
