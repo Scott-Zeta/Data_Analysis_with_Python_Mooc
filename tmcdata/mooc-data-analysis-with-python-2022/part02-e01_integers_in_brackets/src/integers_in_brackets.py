dc089d1ba611ee6b2cc1d1c543722a524a525aa3
Scott-Zeta <wc070211@gmail.com>
2022-11-30 17:08:29 +1030

    error version

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -2,11 +2,25 @@
 import re
 
 def integers_in_brackets(s):
-    s1 = re.findall([.], s)
-    return s1
+    matches = re.findall(r'\[[\s]*([-+]*)(\d+)[\s]*\]', s)
+    print(matches)
+    newTupleList = []
+    for elements in matches:
+        count = 0
+        for i in range(len(elements[0])):
+            if(elements[0][i] == "-"):
+                count += 1
+        if(count%2 == 0):
+            newTupleList.append((1,elements[1]))
+        else:
+            newTupleList.append((-1,elements[1]))
+    print(newTupleList)
+    result = [i[0] * int(i[1]) for i in newTupleList]
+    #print(result)
+    return result
 
 def main():
-    print(integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx"))
+    print(integers_in_brackets("afd [128+] [47 ] [a34]  [ +-43 ]tt [+12]xxx!"))
 
 if __name__ == "__main__":
     main()
