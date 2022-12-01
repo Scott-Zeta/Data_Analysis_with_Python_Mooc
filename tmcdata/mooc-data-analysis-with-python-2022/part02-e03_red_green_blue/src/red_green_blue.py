8cc2560b86c0f2fa7e48279e54ab5dd5d4515255
Scott-Zeta <wc070211@gmail.com>
2022-12-01 16:52:41 +1030

    add a neat solution

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e03_red_green_blue/src/red_green_blue.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e03_red_green_blue/src/red_green_blue.py
@@ -23,6 +23,16 @@ def red_green_blue(filename="src/rgb.txt"):
         count +=1
     return result
 
+# better solution
+def red_green_blue(filename="src/rgb.txt"):
+    with open(filename) as in_file:
+        l = re.findall(r"(\d+)\s+(\d+)\s+(\d+)\s+(.*)\n", in_file.read())
+        return [
+            "{}\t{}\t{}\t{}".format(r, g, b, name)
+            for r, g, b, name
+            in l
+        ]
+
 
 def main():
     red_green_blue()
