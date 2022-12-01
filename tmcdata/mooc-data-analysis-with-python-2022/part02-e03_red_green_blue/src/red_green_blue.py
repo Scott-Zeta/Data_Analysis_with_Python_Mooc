2fb995b686aa7159f1d614fe4b626c68a92275bb
Scott-Zeta <wc070211@gmail.com>
2022-12-01 16:50:15 +1030

    ex3

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e03_red_green_blue/src/red_green_blue.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,29 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    with open(filename,"r") as f:
+        lines = f.readlines()
+    count = 1
+    result = []
+    for i in range(len(lines)):
+        if (i == 0):
+            continue
+        matches = re.match(r"\s*(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})\s+(.+)", lines[i])
+        print(f"{count}: R:{matches.group(1)} G:{matches.group(2)} B:{matches.group(3)} Name:{matches.group(4)}")
+        strElement=""
+        for i in range(1,5):
+            if(i == 4):
+                strElement += f"{matches.group(i)}"
+            else:    
+                strElement += f"{matches.group(i)}\t"
+        print(strElement)
+        result.append(strElement)
+        count +=1
+    return result
 
 
 def main():
-    pass
+    red_green_blue()
 
 if __name__ == "__main__":
     main()
