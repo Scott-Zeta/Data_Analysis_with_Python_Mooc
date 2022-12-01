204d1e3a319ce4662b6f2eb64c3d92332ab50a28
Scott-Zeta <wc070211@gmail.com>
2022-12-01 16:01:42 +1030

    add an easier method to detect numeric elements

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e02_file_listing/src/file_listing.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e02_file_listing/src/file_listing.py
@@ -12,6 +12,12 @@ def file_listing(filename="src/listing.txt"):
         print(f'{count}: {line}',end="")
         matches = re.findall(r'(\d+)\s+(\w{3})\s+(\d{1,2})\s+(\d{2}):(\d{2})\s+([.\w]+.\w+)', line)
         print(matches[0])
+        
+        # another solution of digits string elements to Integer
+        toIntTuple = (int(element) if element.isdecimal() else element for element in matches[0])
+        print(list(toIntTuple))
+        
+        
         intTuple = ()
         for element in matches[0]:
             if element.isnumeric():
