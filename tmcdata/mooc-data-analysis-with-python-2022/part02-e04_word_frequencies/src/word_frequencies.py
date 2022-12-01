0a83eeaea59179fb152ba269b713238025ad379c
Scott-Zeta <wc070211@gmail.com>
2022-12-01 19:24:47 +1030

    ex4

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e04_word_frequencies/src/word_frequencies.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
-def word_frequencies(filename):
-    return {}
+def word_frequencies(filename = "src/alice.txt"):
+    result = {}
+    with open(filename,"r") as f:
+        for line in f:
+            #tips for skip the empty lines
+            if line.rstrip():
+                line = line.split()
+                for word in line:
+                    word = word.strip("""!"#$%&'()*,-./:;?@[]_""")
+                    if word in result:
+                        result[word] += 1
+                    else:
+                        result[word] = 1
+    return result
 
 def main():
-    pass
+    dic = word_frequencies()
+    print(dic)
 
 if __name__ == "__main__":
     main()
