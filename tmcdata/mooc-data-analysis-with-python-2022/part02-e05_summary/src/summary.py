d376bb84d199abd66e962ff0e879946fcd66fe99
Scott-Zeta <wc070211@gmail.com>
2022-12-02 15:15:52 +1030

    ex5

--- tmcdata/mooc-data-analysis-with-python-2022/part02-e05_summary/src/summary.py
+++ tmcdata/mooc-data-analysis-with-python-2022/part02-e05_summary/src/summary.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+from functools import reduce
+import math
+import statistics
 
 def summary(filename):
-    return (0,0,0)
+    dataset = []
+    with open(filename,"r") as f:
+        for line in f:
+            matches = re.match(r"-*\d+.?\d*", line)
+            if(matches != None):
+                dataset.append(matches.group(0))
+        sum, avg, std = calculate(dataset)
+    return (sum,avg,std)
+
+def calculate(dataset):
+    dataset = [float(element) for element in dataset]
+    sum = reduce(lambda x,y: x+y,dataset,0)
+    avg = sum/len(dataset)
+    #std = math.sqrt((reduce(lambda x,y: (x - avg)**2 + y,dataset,0))/(len(dataset)-1))
+    std = statistics.stdev(dataset)
+    return sum,avg,std
 
 def main():
-    pass
+    filename = sys.argv[1:]
+    for name in filename:
+        tup = summary(name)
+        print(f"File: {name} Sum: {tup[0]:.6f} Average: {tup[1]:.6f} Stddev: {tup[2]:.6f}")
 
 if __name__ == "__main__":
     main()
