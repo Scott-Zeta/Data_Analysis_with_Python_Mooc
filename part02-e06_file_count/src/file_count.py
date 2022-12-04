7f651b739c299a73e85c134918198caef8f8b4cf
Scott-Zeta <wc070211@gmail.com>
2022-12-04 19:22:59 +1030

    ex6 part 1

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -3,10 +3,25 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    with open(filename,'r') as f:
+        # if skip the empty lines
+        # content = [l.rstrip() for l in f if l.rstrip()]
+        content = [l for l in f]
+        lines = len(content)
+        words = 0
+        char = 0
+        for l in content:
+            line = l.split()
+            words += len(line)
+            char += len(l)
+        print(lines)
+        print(words)
+        print(char)
+    return (lines, words, char)
 
 def main():
-    pass
+    if(len(sys.argv) > 1):
+        file_count(sys.argv[1])
 
 if __name__ == "__main__":
     main()
