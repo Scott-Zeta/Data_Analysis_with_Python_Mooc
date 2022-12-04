f14aa60348929090136a1b5d74d785f6ad9ee5c0
Scott-Zeta <wc070211@gmail.com>
2022-12-05 08:59:32 +1030

    seperate extention and non-extention

--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+import sys
+import re
+
 def file_extensions(filename):
+    dic = {}
+    li = []
+    with open(filename,"r") as f:
+        count = 1
+        for l in f:
+            l = l.rstrip()
+            print(f"{count}: {l}")
+            count+=1
+            matches = re.search(r'.+\.[^\.]+', l)
+            if(matches != None):
+                ex = l.split(".")[-1]
+                print(ex)
+            else:
+                #non_extention()
+                li.append(l)
+    print(li)
     return ([], {})
 
 def main():
-    pass
+    if(len(sys.argv) > 1):
+        file_extensions(sys.argv[1])
 
 if __name__ == "__main__":
     main()
