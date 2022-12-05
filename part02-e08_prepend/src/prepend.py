971d4f1afd391e55b708c255ce6e33c5cf518330
Scott-Zeta <wc070211@gmail.com>
2022-12-05 18:33:03 +1030

    ex08

--- part02-e08_prepend/src/prepend.py
+++ part02-e08_prepend/src/prepend.py
@@ -2,9 +2,14 @@
 
 class Prepend(object):
     # Add the methods of the class here
-
+    def __init__(self,s):
+        self.s = s
+    
+    def write(self,s):
+        print(f'{self.s}{s}')
 def main():
-    pass
+    p = Prepend("+++ ")
+    p.write("Hello")
 
 if __name__ == "__main__":
     main()
