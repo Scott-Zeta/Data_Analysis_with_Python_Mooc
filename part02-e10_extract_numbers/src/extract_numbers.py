162148a43edae9ee7408f0892ad7534df124e31c
Scott-Zeta <wc070211@gmail.com>
2022-12-12 12:56:37 +1030

    ex10

--- part02-e10_extract_numbers/src/extract_numbers.py
+++ part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def extract_numbers(s):
-    return []
+    li = []
+    input = s.split()
+    for e in input:
+        try:
+            li.append(int(e))
+        except:
+            try:
+                li.append(float(e))
+            except:
+                continue
+    return li
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
