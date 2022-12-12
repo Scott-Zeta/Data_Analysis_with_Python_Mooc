bcf14d9202f6fb96567dbaf8077048b2d8f82d53
Scott-Zeta <wc070211@gmail.com>
2022-12-12 12:09:22 +1030

    ex9

--- part02-e09_rational/src/rational.py
+++ part02-e09_rational/src/rational.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python3
 
 class Rational(object):
-    pass
+    
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
 
+    def __add__(self, other):
+        new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __truediv__(self, other):
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        return Rational(new_numerator, new_denominator)
+
+    def __eq__(self, other):
+        own_fraction = self.numerator/self.denominator
+        other_fraction = other.numerator/other.denominator
+        return own_fraction == other_fraction
+
+    def __gt__(self, other):
+        own_fraction = self.numerator/self.denominator
+        other_fraction = other.numerator/other.denominator
+        return own_fraction > other_fraction
+
+    def __lt__(self, other):
+        own_fraction = self.numerator/self.denominator
+        other_fraction = other.numerator/other.denominator
+        return own_fraction < other_fraction
+
+
+    def __repr__(self):
+        return f"{self.numerator}/{self.denominator}"
 def main():
     r1=Rational(1,4)
     r2=Rational(2,3)
