71537ef70fad74c41eeeed916cf64af287398814
Scott-Zeta <wc070211@gmail.com>
2022-12-13 18:17:25 +1030

    ex13

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -3,10 +3,22 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    dim = n*2 - 1
+    d = np.zeros((dim,dim), dtype = int)
+    trick = np.eye(dim)
+    trick2 = np.arange(2)
+    trick2 = np.concatenate((trick2,trick2))
+    for n in range(d.shape[1]):
+        if (n < int(dim/2)):
+            d[n,int(dim/2)+n] = 1
+            d[n,int(dim/2)-n] = 1
+        else:
+            d[n,int(dim/2)+(dim-n-1)] = 1
+            d[n,int(dim/2)-(dim-n-1)] = 1
+    return d
 
 def main():
-    pass
+    print(diamond(3))
 
 if __name__ == "__main__":
     main()
