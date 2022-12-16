d08b9a3ca7e0b27df0642a34c43d06874dbb236d
Scott-Zeta <wc070211@gmail.com>
2022-12-16 17:02:55 +1030

    ex15

--- part02-e15_vector_angles/src/vector_angles.py
+++ part02-e15_vector_angles/src/vector_angles.py
@@ -4,10 +4,25 @@ import numpy as np
 import scipy.linalg
 
 def vector_angles(X, Y):
-    np.array([])
+     #Find dot product for the numerator
+    prod = np.multiply(X,Y)
+    dotProduct = np.sum(prod)
+
+    #find the norm of both X and Y
+    normX = np.sqrt(np.sum(np.square(X)))
+    normY = np.sqrt(np.sum(np.square(Y)))
+
+    #This gives the whole fraction
+    fraction = dotProduct/(np.multiply(normX,normY))
+    angle = np.arccos(fraction)
+
+    #changing from degrees to radians
+    result = angle*180/np.pi
+    return np.array(result)
 
 def main():
-    np.array([])
-
+    a = np.array([1,2,3])
+    b = np.array([2,3,4])
+    vector_angles(a, b)
 if __name__ == "__main__":
     main()
