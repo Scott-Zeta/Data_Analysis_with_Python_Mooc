97474f3b91851ecbe45008b66e39024efb40a84b
Scott-Zeta <wc070211@gmail.com>
2022-12-17 18:04:50 +1030

    ex2

--- part03-e02_first_half_second_half/src/first_half_second_half.py
+++ part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,10 +3,16 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    m = int(a.shape[1]/2)
+    condition = np.sum(a[:,0:m],axis=1) > np.sum(a[:,m:],axis=1)
+    # print(np.sum(a[:,:m],axis=1))
+    # print(np.sum(a[:,m:],axis=1))
+    return a[condition]
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2],
+              [2, 2, 1, 2]])
+    print(first_half_second_half(a))
 
 if __name__ == "__main__":
     main()
