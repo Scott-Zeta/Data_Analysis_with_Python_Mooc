22495779c5f8ecd54ff0779c9198eabb3483e9e5
Scott-Zeta <wc070211@gmail.com>
2022-12-17 17:37:09 +1030

    ex1

--- part03-e01_column_comparison/src/column_comparison.py
+++ part03-e01_column_comparison/src/column_comparison.py
@@ -3,10 +3,16 @@
 import numpy as np
 
 def column_comparison(a):
-    return np.array([])
+    condition = a[:,1] > a[:,-2]
+    return a[condition]
     
 def main():
-    pass
+    a = np.array([[8,9,3,8,8],
+                [0, 5, 3, 9, 9],
+                [5, 7, 6, 0, 4],
+                [7, 8, 1, 6, 2],
+                [2, 1, 3, 5, 8]])
+    print(column_comparison(a))
 
 if __name__ == "__main__":
     main()
