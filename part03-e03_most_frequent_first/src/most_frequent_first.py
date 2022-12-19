03fb1837c8e6f27d220d1894ebac587f27bc32cf
Scott-Zeta <wc070211@gmail.com>
2022-12-19 15:32:09 +1030

    not correct

--- part03-e03_most_frequent_first/src/most_frequent_first.py
+++ part03-e03_most_frequent_first/src/most_frequent_first.py
@@ -3,10 +3,18 @@
 import numpy as np
 
 def most_frequent_first(a, c):
-    return np.array([])
+    unique,counts = np.unique(a[:,c],return_counts=True)
+    idx = np.flip(np.argsort(counts))
+    print(unique[idx])
+    print(counts[idx])
+    a = a[:,unique[idx]]
+    return a
 
 def main():
-    pass
+    np.random.seed(1)
+    a = np.random.randint(0,9,size=(8,8))
+    print(a)
+    print(most_frequent_first(a,-1))
 
 if __name__ == "__main__":
     main()
