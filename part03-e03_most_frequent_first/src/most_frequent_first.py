fb5d78896325d80322edbe4da60133b084c16b57
Scott-Zeta <wc070211@gmail.com>
2022-12-19 15:49:15 +1030

    ex3

--- part03-e03_most_frequent_first/src/most_frequent_first.py
+++ part03-e03_most_frequent_first/src/most_frequent_first.py
@@ -3,12 +3,25 @@
 import numpy as np
 
 def most_frequent_first(a, c):
-    unique,counts = np.unique(a[:,c],return_counts=True)
-    idx = np.flip(np.argsort(counts))
-    print(unique[idx])
-    print(counts[idx])
-    a = a[:,unique[idx]]
-    return a
+    unique,inverse,counts = np.unique(a[:,c],return_inverse = True,return_counts=True)
+    print(unique)
+    print(inverse)
+    print(counts)
+
+    # This array indicates the count of each element in the original array a[:,c]. 
+    # For example, the first element of the original array is 1, which appears twice in the original array, 
+    # so the first element of the resulting array is 2. The second element of the original array is 2, 
+    # which appears three times in the original array, so the second element of the resulting array is 3, and so on.
+    # CORE
+    print(counts[inverse])
+    # hash the counts on inverse (actually rebuild the inverse with counts elements)
+    idx = np.flip(np.argsort(counts[inverse]))
+    print(idx)
+    # idx = np.flip(np.argsort(counts))
+    # print(unique[idx])
+    # print(counts[idx])
+    # a = a[unique[idx],:]
+    return a[idx]
 
 def main():
     np.random.seed(1)
