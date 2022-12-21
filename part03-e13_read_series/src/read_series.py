0671939b01198d00f1b05c359450d3570b8ea4df
Scott-Zeta <wc070211@gmail.com>
2022-12-22 07:31:46 +1030

    ex13

--- part03-e13_read_series/src/read_series.py
+++ part03-e13_read_series/src/read_series.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
+import pandas as pd
 
 def read_series():
-    return pd.Series()
+    valueList = []
+    indexList = []
+    while(True):
+        userInput = input("Type, asshole: ")
+        if(userInput == ""):
+            break
+        else:
+            try:
+                index, value = userInput.split()
+                valueList.append(value)
+                indexList.append(index)
+            except:
+                print("invalid input")
+    return pd.Series(valueList,index=indexList)
 
 def main():
-    pass
+    print(read_series())
 
 if __name__ == "__main__":
     main()
