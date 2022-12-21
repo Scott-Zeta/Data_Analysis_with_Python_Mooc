9da888e434606c47d3d11852e538b037e101d624
Scott-Zeta <wc070211@gmail.com>
2022-12-22 09:37:24 +1030

    ex15

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+    return pd.Series(s.index,s.values)
 
 def main():
-    return
+    d = { 2001 : "Bush", 2005: "Bush", 2009: "Obama", 2013: "Obama", 2017 : "Trump"}
+    s = pd.Series(d, name="Presidents")
+    print(s)
+    print(s.index)
+    print(s.values)
+    print(inverse_series(s))
 
 if __name__ == "__main__":
     main()
