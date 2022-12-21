e4c9246cac757fbd0794a74bac4954ed3e10ae99
Scott-Zeta <wc070211@gmail.com>
2022-12-21 18:25:54 +1030

    some test and comments

--- part03-e12_radial_fade/src/radial_fade.py
+++ part03-e12_radial_fade/src/radial_fade.py
@@ -10,6 +10,7 @@ def center(a):
 def radial_distance(a):
     h,w,_ = a.shape
     x,y = np.mgrid[:h,:w] #this will generate two coordinate map of the shape
+    # like [[0,1,2] [0,1,2] [0,1,2]] and [[0,0,0] [1,1,1] [2,2,2]]
     centorid = center(a)
     distance = np.sqrt((x-centorid[0])**2 + (y-centorid[1])**2) # use shape map for calculate, avoid for loop
     return distance
@@ -26,8 +27,8 @@ def radial_fade(a):
     return np.array([[]])
 
 def main():
-    print(center(np.zeros((3, 3, 3))))
-    print(radial_distance(np.zeros((3, 3, 3))))
+    print(center(np.zeros((5, 5, 3))))
+    print(radial_distance(np.zeros((5, 5, 3))))
 
 if __name__ == "__main__":
     main()
