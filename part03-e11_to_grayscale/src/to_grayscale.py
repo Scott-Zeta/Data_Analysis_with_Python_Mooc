dde9c57110234ab02154f36339598a5c976be614
Scott-Zeta <wc070211@gmail.com>
2022-12-21 17:20:36 +1030

    to gray

--- part03-e11_to_grayscale/src/to_grayscale.py
+++ part03-e11_to_grayscale/src/to_grayscale.py
@@ -3,10 +3,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+def to_grayscale(img):
+    result = np.dot(img[...,:3],[0.2126, 0.7152, 0.0722])
+    return result
 
 def main():
-    pass
+    image = plt.imread("src/painting.png")
+    print(image.shape)
+    grayImg = to_grayscale(image)
+    plt.imshow(grayImg)
+    plt.show()
 
 if __name__ == "__main__":
     main()
