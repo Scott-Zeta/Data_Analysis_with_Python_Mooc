2f52bd1e2bbcada9f88b36c78235bf60a5d5f9a8
Scott-Zeta <wc070211@gmail.com>
2022-12-21 15:32:32 +1030

    ex10

--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -4,10 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def subfigures(a):
-    pass
+    plt.subplot(1,2,1)
+    plt.plot(a[:,0],a[:,1])
+    plt.subplot(1,2,2)
+    plt.scatter(a[:,0],a[:,1],c=a[:,2],s=a[:,3])
+    plt.show()
 
 def main():
-    pass
+    a = np.random.randint(0,9,size=(4,4))
+    print(a)
+    subfigures(a)
+
 
 if __name__ == "__main__":
     main()
