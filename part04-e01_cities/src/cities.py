8ccc05923c4cf06a8666c8eeb106fa9666c6f2d4
Scott-Zeta <wc070211@gmail.com>
2022-12-24 17:35:15 +1030

    ex01

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -3,7 +3,10 @@
 import pandas as pd
 
 def cities():
-    return None
+    return pd.DataFrame([[643272, 715.48],[279044, 528.03],
+    [231853, 689.59],[223027, 240.35],[201810,3817.52]],
+    index= ["Helsinki","Espoo","Tampere","Vantaa","Oulu"],
+    columns=["Population","Total area"])
     
 def main():
     return
