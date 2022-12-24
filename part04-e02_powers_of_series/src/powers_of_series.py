4b3a349c9d96e222debadbcb907cb5e8335aae53
Scott-Zeta <wc070211@gmail.com>
2022-12-24 19:47:22 +1030

    ex2

--- part04-e02_powers_of_series/src/powers_of_series.py
+++ part04-e02_powers_of_series/src/powers_of_series.py
@@ -3,10 +3,16 @@
 import pandas as pd
 
 def powers_of_series(s, k):
-    return None
+    result = pd.DataFrame()
+    for n in range(1,k+1):
+        col = pd.Series(s**n, name = n)
+        print(col)
+        result = pd.concat([result,col],axis=1)
+    return result
     
 def main():
-    return
+    s = pd.Series([1,2,3,4], index=list("abcd"))
+    print(powers_of_series(s, 3))
     
 if __name__ == "__main__":
     main()
