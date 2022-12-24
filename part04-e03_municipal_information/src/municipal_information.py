1506fa66a586e4ce1eef026ef472fbfc2baea97c
Scott-Zeta <wc070211@gmail.com>
2022-12-24 21:37:14 +1030

    ex03

--- part04-e03_municipal_information/src/municipal_information.py
+++ part04-e03_municipal_information/src/municipal_information.py
@@ -3,6 +3,11 @@
 import pandas as pd
 
 def main():
+    df = pd.read_csv('src/municipal.tsv', sep='\t')
+    print(f"Shape: {df.shape[0]},{df.shape[1]}")
+    print("Columns:")
+    for c in df.columns:
+        print(c)
     return
 
 
