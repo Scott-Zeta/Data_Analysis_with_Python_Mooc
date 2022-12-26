77275f150c68afe4f1657e6aaa24ecdac6b3e826
Scott-Zeta <wc070211@gmail.com>
2022-12-26 16:57:01 +1030

    ex05

--- part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    return None
+    df = pd.read_csv("src/municipal.tsv",index_col="Region 2018",sep="\t")
+    df = df["Akaa":"Äänekoski"]
+    df = df[(df["Share of Swedish-speakers of the population, %"] > 5) & (df["Share of foreign citizens of the population, %"] > 5)]
+    df = df[["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]]
+    print(df)
+    return df
 
 def main():
-    return
+    swedish_and_foreigners()
 
 if __name__ == "__main__":
     main()
