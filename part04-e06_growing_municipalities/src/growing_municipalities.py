8b1b7acbb31f55cc8d3317b640b7e860a13aa090
Scott-Zeta <wc070211@gmail.com>
2022-12-26 17:20:55 +1030

    ex06

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    riseDf = df[df["Population change from the previous year, %"]> 0]
+    print(riseDf)
+    return len(riseDf)/len(df)
 
 def main():
-    return
+    df = pd.read_csv('src/municipal.tsv', sep='\t', index_col=0)
+    df = df["Akaa":"Äänekoski"]
+    rate = growing_municipalities(df)
+    print(f"Proportion of growing municipalities: {rate:.1f}%") #should not have "%" in here
 
 if __name__ == "__main__":
     main()
