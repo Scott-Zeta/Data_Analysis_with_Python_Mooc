b1996497d705b1fb61d1196538f0f0473a322fe2
Scott-Zeta <wc070211@gmail.com>
2022-12-26 19:29:02 +1030

    ex08

--- part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -3,10 +3,13 @@
 import pandas as pd
 
 def subsetting_by_positions():
-    return pd.DataFrame()
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv",sep='\t')
+    result = df.iloc[0:10,2:4]
+    print(result)
+    return result
 
 def main():
-    return
+    subsetting_by_positions()
 
 if __name__ == "__main__":
     main()
