0fee73dd6fac9d5c9ea0b450bca9704685ffd17b
Scott-Zeta <wc070211@gmail.com>
2022-12-26 20:06:44 +1030

    ex10

--- part04-e10_average_temperature/src/average_temperature.py
+++ part04-e10_average_temperature/src/average_temperature.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def average_temperature():
-    return 0.0
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    july = df[df["m"] == 7]
+    average = july.iloc[:,7].mean()
+    print(average)
+    return average
 
 def main():
-    return
+    print(f"Average temperature in July: {average_temperature():.1f}")
 
 if __name__ == "__main__":
     main()
