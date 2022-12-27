6c568941f8a27bbec21b97461e3706be8ebf6759
Scott-Zeta <wc070211@gmail.com>
2022-12-28 01:56:12 +1030

    ex14

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -4,10 +4,17 @@ import pandas as pd
 import numpy as np
 
 def special_missing_values():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df.replace({"New": np.nan, "Re": np.nan},inplace=True)
+    df["LW"] = df["LW"].astype(float)
+    mask = df["Pos"] > df["LW"]
+    seleted = df[mask]
+    print(seleted)
+    print(seleted.shape)
+    return seleted
 
 def main():
-    return
+    special_missing_values()
 
 if __name__ == "__main__":
     main()
