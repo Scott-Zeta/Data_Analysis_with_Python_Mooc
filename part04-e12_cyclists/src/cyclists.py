51e71ef64172ebf4ef33e0e2219c66b0932b0418
Scott-Zeta <wc070211@gmail.com>
2022-12-27 19:37:46 +1030

    ex12

--- part04-e12_cyclists/src/cyclists.py
+++ part04-e12_cyclists/src/cyclists.py
@@ -3,10 +3,16 @@
 import pandas as pd
 
 def cyclists():
-    return None
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv",sep=";")
+    print(df.shape)
+    result = df.dropna(axis = 1, how="all")
+    result = result.dropna(how = "all")
+    print(result.shape)
+    return result
 
 
 def main():
+    cyclists()
     return
     
 if __name__ == "__main__":
