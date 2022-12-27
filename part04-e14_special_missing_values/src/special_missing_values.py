8fb263f5369f40edee5c9422386930b58429e97c
Scott-Zeta <wc070211@gmail.com>
2022-12-28 01:59:18 +1030

    add model solution

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -13,6 +13,15 @@ def special_missing_values():
     print(seleted.shape)
     return seleted
 
+# double mask method from model solution
+def special_missing_values():
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    m = (df["LW"] == "New") | (df["LW"] == "Re")
+    df.loc[m, "LW"] = np.nan
+    df["LW"] = pd.to_numeric(df["LW"])
+    m2 = df["LW"] < df["Pos"]
+    return df[m2]
+
 def main():
     special_missing_values()
 
