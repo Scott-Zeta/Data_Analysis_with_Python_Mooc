baba64c9e0d5d159fb6ffc95f7eca1cac42e4700
Scott-Zeta <wc070211@gmail.com>
2022-12-28 16:48:57 +1030

    ex15

--- part04-e15_last_week/src/last_week.py
+++ part04-e15_last_week/src/last_week.py
@@ -8,29 +8,15 @@ def last_week():
     df.drop(df[(df['LW'] == "New") | (df['LW'] == "Re")].index, inplace=True)
     df["WoC"] -= 1
     df["LW"] = df["LW"].astype(int)
+    # reach the peak on this week, since can not confirm history position, subsitute with NA
+    # for position, smaller the better, fucking df['Pos'] < df['Lw'] means its increase this week
+    df.loc[(df['Peak Pos'] == df["Pos"]) & (df['Pos'] < df['LW']), 'Peak Pos'] = np.nan
     df = df.sort_values(by=['LW'])
     df.index = df["LW"]
     df = df.reindex(range(1,41)) # fill NA with missing row
     df["Pos"] = df.index
     df["LW"] = np.nan
     print(df)
-    return df
-
-# def last_week():
-#     df = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')    
-#     df['WoC'] -= 1
-#     df = df[~df['LW'].isin(['New', 'Re'])]
-#     df['LW']=df['LW'].astype(int)
-    
-    # mask1 = df['Peak Pos'] == df['Pos']
-    # mask2 = df['Pos'] < df['LW']
-    # mask = mask1 & mask2
-    # df.loc[mask, 'Peak Pos'] = np.nan
-    # df = df.sort_values(by=['LW'])
-    # df.index = df['LW']
-    # df = df.reindex(range(1,41))
-    # df['Pos'] = df.index
-    # df['LW'] = np.nan
     return df
 
 def main():
