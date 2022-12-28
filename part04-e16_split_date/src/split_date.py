738aed51fb9e8e073daeb52fa4291c78978176f3
Scott-Zeta <wc070211@gmail.com>
2022-12-28 19:05:23 +1030

    ex16

--- part04-e16_split_date/src/split_date.py
+++ part04-e16_split_date/src/split_date.py
@@ -5,10 +5,24 @@ import numpy as np
 
 
 def split_date():
-    return None
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    date = df["Päivämäärä"].str.split(expand=True)
+    date[[4,5]] = date[4].str.split(":",expand=True)
+    date = date.drop(5,axis=1)
+    date = date.dropna()
+    date = date.rename(columns={0:"Weekday",1:"Day",2:"Month",3:"Year",4:"Hour"})
+    weekdayDic = {"ma":"Mon","ti":"Tue","ke":"Wed","to":"Thu","pe":"Fri","la":"Sat","su":"Sun"}
+    monthDic = {"tammi":1,"helmi":2,"maalis":3,"huhti":4,"touko":5,"kesä":6,"heinä":7,"elo":8,
+                "syys":9,"loka":10,"marras":11,"joulu":12}
+    date['Weekday'] = date["Weekday"].map(weekdayDic)
+    date['Month'] = date["Month"].map(monthDic).map(int)
+    date[['Day','Year','Hour']] = date[['Day','Year','Hour']].astype(int)
+    print(date)
+    print(date.dtypes)
+    return date
 
 def main():
-    return
+    split_date()
        
 if __name__ == "__main__":
     main()
