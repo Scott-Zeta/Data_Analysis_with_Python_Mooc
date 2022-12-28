acf8e4bd294cbb5049136b2841ca63df9db0a91a
Scott-Zeta <wc070211@gmail.com>
2022-12-28 16:38:16 +1030

    working on peak positon

---
+++ part04-e15_last_week/src/README.md
@@ -0,0 +1,6 @@
+## what need to be deal for rebuild last weeks table?
+1. Pos to LW
+2. LW to N/A
+3. WOC - 1
+4. probably PEAK POS
+5. Some special case
--- part04-e15_last_week/src/last_week.py
+++ part04-e15_last_week/src/last_week.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def last_week():
-    return pd.DataFrame()
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df.drop(df[(df['LW'] == "New") | (df['LW'] == "Re")].index, inplace=True)
+    df["WoC"] -= 1
+    df["LW"] = df["LW"].astype(int)
+    df = df.sort_values(by=['LW'])
+    df.index = df["LW"]
+    df = df.reindex(range(1,41)) # fill NA with missing row
+    df["Pos"] = df.index
+    df["LW"] = np.nan
+    print(df)
+    return df
+
+# def last_week():
+#     df = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')    
+#     df['WoC'] -= 1
+#     df = df[~df['LW'].isin(['New', 'Re'])]
+#     df['LW']=df['LW'].astype(int)
+    
+    # mask1 = df['Peak Pos'] == df['Pos']
+    # mask2 = df['Pos'] < df['LW']
+    # mask = mask1 & mask2
+    # df.loc[mask, 'Peak Pos'] = np.nan
+    # df = df.sort_values(by=['LW'])
+    # df.index = df['LW']
+    # df = df.reindex(range(1,41))
+    # df['Pos'] = df.index
+    # df['LW'] = np.nan
+    return df
 
 def main():
     df = last_week()
