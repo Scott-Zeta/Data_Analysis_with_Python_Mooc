64c45f41a538434c9ad8bead968bad816c807255
Scott-Zeta <wc070211@gmail.com>
2023-01-04 05:54:22 +1030

    update lambda function and non-lambda method

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -5,7 +5,8 @@ import pandas as pd
 def top_bands():
     df_UK40 = pd.read_csv("src/UK-top40-1964-1-2.tsv",sep="\t")
     df_Bands = pd.read_csv("src/bands.tsv",sep="\t")
-    df_Bands.loc[:,["Band"]] = df_Bands.loc[:,["Band"]].apply(lambda x: x.str.upper())
+    df_Bands["Band"] = df_Bands["Band"].str.upper()
+    df_Bands.loc[:,["Band"]] = df_Bands.loc[:,["Band"]].apply(lambda x: x.str.upper() if x.dtype == "object" else x)
     result = pd.merge(df_UK40,df_Bands,left_on=["Artist"],right_on=["Band"])
     return result
 
