294d878e2a1e039911b424a49dadbdd6a13e5037
Scott-Zeta <wc070211@gmail.com>
2023-01-04 05:51:03 +1030

    ex03

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -3,9 +3,15 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
+    df_UK40 = pd.read_csv("src/UK-top40-1964-1-2.tsv",sep="\t")
+    df_Bands = pd.read_csv("src/bands.tsv",sep="\t")
+    df_Bands.loc[:,["Band"]] = df_Bands.loc[:,["Band"]].apply(lambda x: x.str.upper())
+    result = pd.merge(df_UK40,df_Bands,left_on=["Artist"],right_on=["Band"])
+    return result
 
 def main():
+    result = top_bands()
+    print(result)
     return
 
 if __name__ == "__main__":
