c8c0f7a8d1367d0ce8262e8b587bf95d2e6d4b36
Scott-Zeta <wc070211@gmail.com>
2023-01-05 18:37:37 +1030

    ex05

--- part05-e05_best_record_company/src/best_record_company.py
+++ part05-e05_best_record_company/src/best_record_company.py
@@ -3,9 +3,14 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep = "\t")
+    print(df)
+    result = df.groupby('Publisher')['WoC'].sum().sort_values(ascending=False)
+    mask = result.index[0]
+    return df[df['Publisher'] == mask]
 
 def main():
+    print(best_record_company())
     return
     
 
