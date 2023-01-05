1656b6edb896ee4685e489116edf85e9aed1be2f
Scott-Zeta <wc070211@gmail.com>
2023-01-05 20:00:15 +1030

    ex06

--- part05-e06_suicide_fractions/src/suicide_fractions.py
+++ part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -3,9 +3,16 @@
 import pandas as pd
 
 def suicide_fractions():
-    return pd.Series()
+    df = pd.read_csv("src/who_suicide_statistics.csv")
+    df.drop(['year','sex','age'],axis = 1,inplace=True)
+    df['rate'] = df['suicides_no'] / df['population']
+    group = df.groupby('country')
+    print(group.get_group("United States of America"))
+    result = group['rate'].mean()
+    return result
 
 def main():
+    print(suicide_fractions())
     return
 
 if __name__ == "__main__":
