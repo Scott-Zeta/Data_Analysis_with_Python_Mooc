5bc4d214c7c65007542bc56e02171ed967aa622a
Scott-Zeta <wc070211@gmail.com>
2023-01-09 09:38:10 +1030

    ex12

--- part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -5,9 +5,24 @@ from sklearn import linear_model
 
 
 def coefficient_of_determination():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv",sep="\t")
+    model = linear_model.LinearRegression(fit_intercept=True)
+    x = df.iloc[:,0:5]
+    y = df.iloc[:,5]
+    model.fit(x,y)
+    R2 = []
+    R2.append(model.score(x,y))
+    for column in x:
+        sub_X = df.loc[:,column]
+        sub_X = sub_X.values.reshape(-1,1)
+        model.fit(sub_X,y)
+        R2.append(model.score(sub_X,y))
+    return R2
     
 def main():
+    R2 = coefficient_of_determination()
+    print(R2)
+    return
 
 if __name__ == "__main__":
     main()
