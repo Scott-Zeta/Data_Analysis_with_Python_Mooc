db884fd9dfd9c052a5ee9ea570fc2a0f3d353b13
Scott-Zeta <wc070211@gmail.com>
2023-01-08 17:38:38 +1030

    ex10 part1

--- part05-e10_linear_regression/src/linear_regression.py
+++ part05-e10_linear_regression/src/linear_regression.py
@@ -3,11 +3,24 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import random
 
 def fit_line(x, y):
-    return 0, 0
+    model = LinearRegression(fit_intercept=True)
+    model.fit(x[:,np.newaxis], y)
+    return model.coef_[0], model.intercept_
     
 def main():
+    ran_slope = random.uniform(-3,3)
+    ran_inter = random.uniform(-5,5)
+    n = 11
+    x = np.linspace(0,10,n)
+    y = x*ran_slope + ran_inter + np.random.randn(n)
+    # print(ran_slope)
+    # print(ran_inter)
+    slope, intercept = fit_line(x,y)
+    print(f"Slope: {slope}")
+    print(f"Intercept: {intercept}")
     
 if __name__ == "__main__":
     main()
