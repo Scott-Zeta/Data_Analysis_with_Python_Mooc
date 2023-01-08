d244637686cb65252dde9d1bc173b755c0dded9d
Scott-Zeta <wc070211@gmail.com>
2023-01-08 17:52:44 +1030

    part2 finished

--- part05-e10_linear_regression/src/linear_regression.py
+++ part05-e10_linear_regression/src/linear_regression.py
@@ -8,7 +8,7 @@ import random
 def fit_line(x, y):
     model = LinearRegression(fit_intercept=True)
     model.fit(x[:,np.newaxis], y)
-    return model.coef_[0], model.intercept_, model
+    return model.coef_[0], model.intercept_
     
 def main():
     ran_slope = random.uniform(-3,3)
@@ -18,11 +18,12 @@ def main():
     y = x*ran_slope + ran_inter + np.random.randn(n)
     # print(ran_slope)
     # print(ran_inter)
-    slope, intercept, model = fit_line(x,y)
+    slope, intercept = fit_line(x,y)
     print(f"Slope: {slope}")
     print(f"Intercept: {intercept}")
     plt.plot(x,y,'o')
-    plt.plot(x,model.predict(x[:,np.newaxis]),color = 'red')
+    predict_value = [slope*i + intercept for i in x]
+    plt.plot(x,predict_value,color = 'red')
     plt.show()
     
 if __name__ == "__main__":
