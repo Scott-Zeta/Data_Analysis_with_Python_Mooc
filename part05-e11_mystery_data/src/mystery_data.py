43fe337f0e3d9f4198b255758e587021d6b4b800
Scott-Zeta <wc070211@gmail.com>
2023-01-08 19:09:57 +1030

    ex11

--- part05-e11_mystery_data/src/mystery_data.py
+++ part05-e11_mystery_data/src/mystery_data.py
@@ -2,12 +2,22 @@
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 def mystery_data():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv",sep="\t")
+    model = LinearRegression(fit_intercept=False)
+    x = df.iloc[:,0:5]
+    y = df.iloc[:,5]
+    # print(x)
+    # print(y)
+    model.fit(x,y)
+    print(df.head())
+    return [x for x in model.coef_]
 
 def main():
     coefficients = mystery_data()
+    print(coefficients)
     # print the coefficients here
     
 if __name__ == "__main__":
