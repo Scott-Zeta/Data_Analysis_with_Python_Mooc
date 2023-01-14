ae539cc22f9531ff3da3b434328dc7c89abc6adf
Scott-Zeta <wc070211@gmail.com>
2023-01-14 21:10:25 +1030

    ex06 part1

--- part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
+++ part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
@@ -4,9 +4,22 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import accuracy_score
+import scipy
 
+# function for label permutation
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
 
 def nonconvex_clusters():
+    # read dataframe
+    df = pd.read_csv("src/data.tsv",sep="\t")
+    print(df)
     return pd.DataFrame()
 
 def main():
