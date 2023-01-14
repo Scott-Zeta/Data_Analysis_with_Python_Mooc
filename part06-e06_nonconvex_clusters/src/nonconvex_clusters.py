5d6ca0b31dbc183227e57c9a970c47e9d3ea6aec
Scott-Zeta <wc070211@gmail.com>
2023-01-15 09:51:39 +1030

    part2

--- part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
+++ part06-e06_nonconvex_clusters/src/nonconvex_clusters.py
@@ -19,7 +19,21 @@ def find_permutation(n_clusters, real_labels, labels):
 def nonconvex_clusters():
     # read dataframe
     df = pd.read_csv("src/data.tsv",sep="\t")
-    print(df)
+    X = df.iloc[:,0:2]
+    y = df.iloc[:,2]
+    for n in np.arange(0.05,0.2,0.05):
+        model = DBSCAN(eps=n)
+        model.fit(X)
+        # -1 in cluster label means the noisy point
+        n_cluster = len(np.unique(model.labels_)) - (1 if -1 in model.labels_ else 0)
+        if n_cluster == len(np.unique(y)):
+            permutation = find_permutation(n_cluster,y,model.labels_)
+            new_labels = [permutation[label] for label in model.labels_]
+            acc = accuracy_score(y,new_labels)
+        else:
+            acc = np.nan
+        print(f'{n}:{acc}')
+
     return pd.DataFrame()
 
 def main():
