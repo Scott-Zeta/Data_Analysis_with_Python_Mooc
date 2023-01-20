ec466b471a4480dfe03ad2659989029c23b48989
Scott-Zeta <wc070211@gmail.com>
2023-01-20 15:27:32 +1030

    ex08 part2 completed

--- part06-e08_explained_variance/src/explained_variance.py
+++ part06-e08_explained_variance/src/explained_variance.py
@@ -2,21 +2,28 @@
 
 from sklearn.decomposition import PCA
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 def explained_variance():
     df = pd.read_csv('src/data.tsv', sep = '\t')
     variance = df.var()
-    print(variance)
+    # print(variance)
     pcaModel = PCA()
     pcaModel.fit(df)
     ex_variance = pcaModel.explained_variance_
-    print(ex_variance)
+    # print(ex_variance)
     return variance, ex_variance
 
 def main():
     v, ev = explained_variance()
     print(sum(v), sum(ev))
+    vlist = " ".join(f'{x:.3f}' for x in v)
+    evlist = " ".join(f'{x:.3f}' for x in ev)
+    print(f"The variances are: {vlist}")
+    print(f"The explained variances after PCA are: {evlist}")
+    plt.plot(np.arange(1,11), np.cumsum(ev))
+    plt.show()
 
 if __name__ == "__main__":
     main()
